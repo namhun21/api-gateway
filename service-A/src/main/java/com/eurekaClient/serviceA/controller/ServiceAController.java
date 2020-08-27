5bb818eaae3a65cde7ecb1baf60269c4792827a3
namhun21 <namhun21@naver.com>
2020-08-27 14:23:14 +0900

    주석 추가

--- service-A/src/main/java/com/eurekaClient/serviceA/controller/ServiceAController.java
+++ service-A/src/main/java/com/eurekaClient/serviceA/controller/ServiceAController.java
@@ -8,20 +8,20 @@ import java.util.Map;
 @RestController
 public class ServiceAController {
 
-    @GetMapping("/api")
+    @GetMapping("/api") //get방식으로 보낼 때
     public String getRequest() {
         return "Hello Service A";
     }
 
     @GetMapping(value = "/api/{idx}")
-    @HystrixCommand(fallbackMethod = "doFallBack")
+    @HystrixCommand(fallbackMethod = "doFallBack") //circuit breaker 역할을 수행할 메소드 에러 발생 시 doFallBack()메소드로 이동
     public String getRequest(@PathVariable("idx") String idx) {
-        if (idx.equalsIgnoreCase("error"))
+        if (idx.equalsIgnoreCase("error")) //대소문자 관계없이 비교
             throw new RuntimeException();
         return "Hello Service A";
     }
 
-    @PostMapping("/api")
+    @PostMapping("/api") //post방식으로 전송
     @HystrixCommand(fallbackMethod = "doFallBack")
     public String postRequest(@RequestBody String message) {
         if (message.equalsIgnoreCase("error"))
--- service-B/src/main/java/com/eurekaClient/serviceB/Controller/ServiceBController.java
+++ service-B/src/main/java/com/eurekaClient/serviceB/Controller/ServiceBController.java
@@ -12,17 +12,18 @@ import java.util.Map;
 @RestController
 public class ServiceBController {
 
-    @GetMapping("/api")
+    @GetMapping("/api") //get방식
     public String getRequest(){
         return "Hello Service B";
     }
     @GetMapping(value = "/api/{idx}")
-    @HystrixCommand(fallbackMethod = "doFallBack")
+    @HystrixCommand(fallbackMethod = "doFallBack") //circuit breaker 역할을 수행할 method
     public String getRequest(@PathVariable("idx") String idx){
         if (idx.equalsIgnoreCase("error"))
             throw new RuntimeException();
         return "Hello ServiceB";
     }
+
     @PostMapping("/api")
     @HystrixCommand(fallbackMethod = "doFallBack")
     public String postRequest(@RequestBody String message){
@@ -31,7 +32,7 @@ public class ServiceBController {
         return message;
     }
 
-    public String doFallBack(@RequestBody String message) {
+    public String doFallBack(@RequestBody String message) { //에러 발생시 해당 메소드 실행
         return "Fall Back";
     }
 }
